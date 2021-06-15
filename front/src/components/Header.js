13503822740b49a08bd6ff2330864d52c6325905
kiyosumi-png <shoudaoqingcheng3@gmail.com>
2021-06-15 18:10:37 +0900

    create Header

---
+++ front/src/components/Header.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { Menu } from './header/index';
+
+function Header() {
+    return (
+        <Container>
+            <Menu />
+        </Container>
+    );
+}
+
+export default Header;
+
+const Container = styled.div`
+    display: flex;
+    max-height: 60px;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 10;
+    background-color: #fff6d9;
+`;
--- front/src/components/header/index.js
+++ front/src/components/header/index.js
@@ -1,3 +1,6 @@
 export { default as Button } from './Button';
 export { default as Icon } from './Icon';
 export { default as Input } from './Input';
+// 原子
+
+export { default as Menu } from './menu/Menu';
